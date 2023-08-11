e534423af19cd7e95e71154424baf8729e61eeba
dottling <igor.dottling@outlook.com>
2023-08-11 14:39:05 +0200

    created readme file, removed pointsArray and overall cleaning

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Useful Commands
+
+Here are some useful commands to help you get started and work with this codebase:
+
+### Accessing dataArray
+
+`dataArray[i].dataX[j]`
+`dataArray[i].dataY[j]`
--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const root = document.querySelector(':root');
 const cartesianPlane = document.getElementById('cartesianPlane');
 const inputForm = document.getElementById('inputForm');
 
-
 const cartesianPlaneRect = cartesianPlane.getBoundingClientRect();
 
-const zeroPlaneX =
-  parseFloat(cartesianPlaneRect.height)/2;
-const zeroPlaneY =
-  parseFloat(cartesianPlaneRect.width)/2;
-const pointsArray = [];
+const zeroPlaneX = parseFloat(cartesianPlaneRect.height) / 2;
+const zeroPlaneY = parseFloat(cartesianPlaneRect.width) / 2;
+
+// Array that holds all data points that the user provided (both as a click or form)
+// Each entry is an object that holds 2 arrays (dataX and dataY)
+// dataX and dataY both are arrays of Strings
+// dataArray -> element.dataX, element.dataY
 const dataArray = [];
 
+// Find coordinates based on their position on the cartesian plane
 const findCoordinates = function (x, y) {
   xa = parseFloat(((x - zeroPlaneX) / 20).toFixed(2));
   ya = parseFloat(((zeroPlaneY - y) / 20).toFixed(2));
@@ -20,15 +22,17 @@ const findCoordinates = function (x, y) {
   return coordinates;
 };
 
+// Add X, Y points to the plane after submitting the form.
+// Also save the points on dataArray
 const getData = function (event) {
   event.preventDefault();
 
-  inputX = document.getElementById('inputX').value.split(',');
-  inputY = document.getElementById('inputY').value.split(',');
+  let inputX = document.getElementById('inputX').value.split(',');
+  let inputY = document.getElementById('inputY').value.split(',');
 
   const data = {
-    dataX: [inputX],
-    dataY: [inputY],
+    dataX: inputX,
+    dataY: inputY,
   };
 
   dataArray.push(data);
@@ -36,19 +40,17 @@ const getData = function (event) {
   for (let i = 0; i < inputX.length; i++) {
     const point = document.createElement('div');
     point.classList.add('point');
-    x = parseFloat(inputX[i]) * 20 + zeroPlaneX;
-    y = -parseFloat(inputY[i]) * 20 + zeroPlaneY;
+    let x = parseFloat(inputX[i]) * 20 + zeroPlaneX;
+    let y = -parseFloat(inputY[i]) * 20 + zeroPlaneY;
     // console.log(x, y);
     point.style.left = `${x - 4}px`;
     point.style.top = `${y - 4}px`;
     cartesianPlane.appendChild(point);
   }
-
-  // console.log(dataArray, dataArray);
-  console.log(dataArray);
 };
 
 // Add a point to the Cartesian plane on click and console.log coordinates
+// Also save the points on dataArray
 function addPoint(event) {
   const point = document.createElement('div');
   point.classList.add('point');
@@ -57,14 +59,21 @@ function addPoint(event) {
   point.style.left = `${x - 4}px`;
   point.style.top = `${y - 4}px`;
   cartesianPlane.appendChild(point);
-  pointsArray.push(findCoordinates(x, y));
-  console.log(`coordinates: (${findCoordinates(x, y)})`);
 
-  // console.log(typeof pointsArray[0][0]);
-  // console.log(`All points: ${JSON.stringify(pointsArray)}`);
-  // console.log(`(${zeroPlaneX}, ${zeroPlaneY})`);
+  let coord = findCoordinates(x, y);
+  let inputX = String(coord[0]);
+  let inputY = String(coord[1]);
+
+  const data = {
+    dataX: [inputX],
+    dataY: [inputY],
+  };
+
+  dataArray.push(data);
+  console.log(`coordinates: (${findCoordinates(x, y)})`);
 }
 
 // Attach click event listener to the Cartesian plane
 cartesianPlane.addEventListener('click', addPoint);
+// Attach submit event listener to the form button
 inputForm.addEventListener('submit', getData);
